47b0caad3027d088d10860860289e025601d9d33
Mosiur <dum.silor@gmail.com>
2024-11-04 22:43:29 +0600

    only backend done for get books

---
+++ backend/src/books.ts
@@ -0,0 +1,7 @@
+export const sample_books = [
+{
+    id: "1",
+    name: "Test book name",
+    genera: "Test Genera",
+},
+]
---
+++ backend/src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+// import * as fs from 'node:fs';
+import { sample_books } from "./books";
+const app = express();
+
+const corsOptions = {
+    // origin: 'http://localhost:4200', // Allow only this origin
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
+    // credentials: true, // Allow cookies to be sent and received
+  };
++
+app.use(express.json())
+app.use(cors(corsOptions))
+
+app.get("/ip",(req,res)=>{
+    res.send(`You are visiting from ${req.socket.remoteAddress}`)
+})
+
+app.get("/api/books",(_req,res)=>{
+    res.send(sample_books)
+})1
+
+// app.post("/api/books/:id",(req,res){
+//     const body = req.body['id']
+// })
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`App started on port http://localhost:${PORT}`)
+})
