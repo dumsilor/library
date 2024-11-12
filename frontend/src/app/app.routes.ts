02dd3951b016f52352d84d84efb714f6c16e7e86
Mosiur Rahman <mosiur.rahman@novotel-bd.com>
2024-11-12 10:46:56 +0600

    Added Auth Page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FrontPageComponent } from './features/front-page/front-page.component';
 import { BookComponent } from './features/book/book.component';
+import { UserAuthComponent } from './features/user-auth/user-auth.component';
 
 export const routes: Routes = [
     {path: "", component: FrontPageComponent},
-    {path: ":id", component:BookComponent, pathMatch: "prefix"}
+    {path: ":id", component:BookComponent},
+    {path: "auth", component: UserAuthComponent}
 ];
